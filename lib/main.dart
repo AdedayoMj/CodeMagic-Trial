501d68f940b0fbac734de0405308d73aa2ff71c5
ADEDAYO ADEGBOYE <adedayoadegboye@ADEDAYOs-Air.lan>
2021-04-20 14:10:02 +0300

    firebase cli implementation

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,22 @@
+import 'package:firebase_analytics/firebase_analytics.dart';
+import 'package:firebase_analytics/observer.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
 import 'package:trialdayflutter/src/home/home.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  // await Firebase.initializeApp();
-  // await FirebaseCrashlytics.instance.setCrashlyticsCollectionEnabled(true);
+  await Firebase.initializeApp();
+  //await FirebaseCrashlytics.instance.setCrashlyticsCollectionEnabled(true);
   // FlutterError.onError = FirebaseCrashlytics.instance.recordFlutterError;
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  static FirebaseAnalytics analytics = FirebaseAnalytics();
+  static FirebaseAnalyticsObserver observer =
+      FirebaseAnalyticsObserver(analytics: analytics);
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
@@ -20,6 +26,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
+      navigatorObservers: <NavigatorObserver>[observer],
       home: HomePage(),
     );
   }
